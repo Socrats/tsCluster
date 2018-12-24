92e156ddf28dcf34073ac3c2ee873a24e4abe301
eliasfernandez <eliasfernandez.d@gmail.com>
2018-12-24 07:43:14 +0100

    crfsunsupervised converges but has more error than in paper.

--- crfs/crfsUnsupervised.pyx
+++ crfs/crfsUnsupervised.pyx
@@ -26,109 +26,104 @@ crfsUnsupervised. CRFs unsupervised clustering of time-series
 import numpy as np
 cimport numpy as np
 from libcpp cimport bool
-DTYPE = np.float64
+
 ctypedef np.float_t DTYPE_t
 ctypedef np.int_t INTYPE_t
 
 
-cdef np.ndarray[INTYPE_t] get_voting_pool(np.ndarray[DTYPE_t, ndim=2] pool, np.ndarray[INTYPE_t] MS, INTYPE_t MD, int focal, int k, int s):
+# cdef np.ndarray[DTYPE_t] euclidean_dist(np.ndarray[DTYPE_t] t1, np.ndarray[DTYPE_t, ndim=2] t2):
+#     return np.sqrt(np.sum((t1-t2)**2, axis=1))
+
+
+cdef np.ndarray[INTYPE_t] update_voting_pool(np.ndarray[DTYPE_t, ndim=2] pool, np.ndarray[INTYPE_t] voting_pool,
+                                             np.ndarray[INTYPE_t] ts_index, int focal, int k, int s):
     """
     The voting pool is formed by selecting s MS time series, 1 MD
     and k-s-1 time series selcted at random
     """
     assert k > 1
     assert s > 0
-
-    ts_index = np.arange(pool.shape[0])
+    cdef np.ndarray[DTYPE_t] distances
 
     # First we get k-s-1 random time-series
-    voting_pool = np.random.choice(ts_index[(ts_index != MD) & (ts_index != focal) & np.isin(ts_index, MS, invert=True)], size=k-s-1, replace=False)
-    voting_pool = np.append([MD], np.append(MS, voting_pool))
+    voting_pool[s:-1] = np.random.choice(
+        ts_index[(ts_index != voting_pool[-1]) & np.isin(ts_index, voting_pool[:s], invert=True)],
+        size=k - s - 1, replace=False)
 
     # Then we find s most similar (MS) time-series and the most differnt (MD)
     distances = np.linalg.norm(pool[focal] - pool[voting_pool], axis=1)
-    MS[:] = voting_pool[distances.argsort()[:s]]
-    MD = voting_pool[np.argmax(distances)]
-    voting_pool = np.append(voting_pool, [focal])
+    voting_pool[:] = voting_pool[distances.argsort()]
 
     return voting_pool
 
-cdef INTYPE_t check_labels(np.ndarray[DTYPE_t, ndim=2] pool, np.ndarray[INTYPE_t] labels, np.ndarray[INTYPE_t] voting_pool, int m):
+cdef INTYPE_t check_labels(np.ndarray[DTYPE_t, ndim=2] pool, np.ndarray[INTYPE_t] labels,
+                           np.ndarray[INTYPE_t] voting_pool, int m, DTYPE_t D):
     """
     Calculate the potentials of each member of the voting pool with respect to the focal player
     """
-    cdef int i,j
-    cdef int nvpool = voting_pool.shape[0]
-    cdef DTYPE_t dp = 0.
-    cdef DTYPE_t do = 0.
-    cdef DTYPE_t D = 0.
-    cdef np.ndarray[DTYPE_t] costs = np.zeros(shape=(nvpool,));
-    cdef np.ndarray[INTYPE_t] indexes;
+    cdef int i, j
+    cdef int nvpool = len(voting_pool)
+    cdef np.ndarray[INTYPE_t] unique_labels = np.unique(labels)
+    cdef int nulabels = len(unique_labels)
+    cdef np.ndarray[DTYPE_t] costs = np.zeros(shape=(nulabels,))
+    cdef np.ndarray[DTYPE_t] distances
 
-    # First we infer D
-    for i in voting_pool:
-        # get m random members
-        tmp = np.random.choice(voting_pool, size=m, replace=False)
-        dst = np.linalg.norm(pool[i] - pool[tmp], axis=1)
-        dp += np.min(dst)
-        do += np.max(dst)
-    D = (dp + do) / DTYPE(2. * nvpool)
+    # Calculate the costs of each label
+    for index1 in range(nulabels):
+        for i in voting_pool[labels == unique_labels[index1]]:
+            distances = np.linalg.norm(pool[i] - pool[voting_pool], axis=1) - D
+            for index2, j in enumerate(voting_pool):
+                if i == j: continue
+                costs[index1] += distances[index2] if (unique_labels[index1] == labels[index2]) else -distances[index2]
+    return unique_labels[np.argmin(costs)]
 
-    # Calculate the costs
-    for index1, i in enumerate(voting_pool):
-        distances = np.linalg.norm(pool[i] - pool[voting_pool], axis=1) - D
-        for index2, j in enumerate(voting_pool):
-            if i==j: continue
-            costs[index1] += distances[index2] if (labels[i] == labels[j]) else -distances[index2]
-    return labels[voting_pool[np.argmin(costs)]]
-
-cpdef np.ndarray[INTYPE_t] cluster_ts(np.ndarray[DTYPE_t, ndim=2] ts, np.ndarray[INTYPE_t] labels, int k, int s, int m, bool inplace=False):
+cpdef np.ndarray[INTYPE_t] cluster_ts(np.ndarray[DTYPE_t, ndim=2] ts, np.ndarray[INTYPE_t] labels, int k, int s, int m,
+                                      int max_iterations=50, bool inplace=False):
     """
     ts: time-series samples
     k: size of the voting pool
     s: number of most similar players selected for the voting pool
     m: number of time-series to which the distance is compared
     """
-    cdef int i=0;
-    cdef int iterations=0;
+    cdef int i = 0;
+    cdef int iterations = 0;
     cdef int new_label;
-    cdef int nts = ts.shape[0]
-    cdef bool label_updated = True;
-    cdef DTYPE_t dp = 0.
-    cdef DTYPE_t do = 0.
+    cdef int nts = len(ts)
+    cdef DTYPE_t d = 0.
     cdef DTYPE_t D = 0.
-    cdef np.ndarray[INTYPE_t, ndim=1] voting_pool;
-    cdef np.ndarray[INTYPE_t, ndim=2] MS = np.zeros(shape=(nts, s), dtype=np.int64);
-    cdef np.ndarray[INTYPE_t, ndim=1] MD = np.zeros(shape=(nts,), dtype=np.int64);
+    cdef bool label_updated = True;
+    cdef np.ndarray[INTYPE_t, ndim=2] voting_pool = np.zeros(shape=(nts, k), dtype=np.int64)
+    cdef np.ndarray[INTYPE_t] ts_index = np.arange(nts)
+
     if inplace:
         labels_copy = labels
     else:
         labels_copy = labels.copy()
 
-    # First we initialize MS and MD
-    ts_index = np.arange(nts)
     for i in range(nts):
-        tmp = np.random.choice(ts_index[ts_index != i], size=s+1, replace=False)
-        MS[i, :] = tmp[:s]
-        MD[i] = tmp[-1]
+        voting_pool[i, :] = np.random.choice(ts_index[ts_index != i], size=k, replace=False)
+        # get m random members
+        tmp = np.random.choice(voting_pool[voting_pool != i], size=m, replace=False)
+        dst = np.linalg.norm(ts[i] - ts[tmp], axis=1)
+        d += np.min(dst) + np.max(dst)
+    D = d / (2. * nts)
 
     while label_updated:
+        # print('[', iterations, '] label 0: ', labels_copy[0])
         label_updated = False
         # First let's calculate the potentials
         for i in range(nts):
             # create the voting pool
-            voting_pool = get_voting_pool(ts, MS[i, :], MD[i], i, k, s)
-            MD[i] = voting_pool[0]
-            MS[i, :] = voting_pool[1:s+1]
+            voting_pool[i, :] = update_voting_pool(ts, voting_pool[i], ts_index, i, k, s)
             # Calculate the cost functions of the labels of the members in the voting pool
-            new_label = check_labels(ts, labels_copy, voting_pool, m)
+            new_label = check_labels(ts, labels_copy[voting_pool[i]], voting_pool[i], m, D)
             # Check if the label has been updated
             if new_label != labels_copy[i]:
                 label_updated = True
                 labels_copy[i] = new_label
         iterations += 1
         if iterations % 10 == 0:
-            print('Iteration: ',iterations)
-        if iterations >= 50: break
+            print('Iteration: ', iterations)
+        if iterations >= max_iterations: break
 
     return labels_copy
--- examples/crf_unsupervised.py
+++ examples/crf_unsupervised.py
@@ -2,17 +2,17 @@ import numpy as np
 from crfs.crfsUnsupervised import cluster_ts
 
 mus = [[40, 45, 100],
-        [85, 60, 100],
-        [65, 100, 80],
-        [55, 120, 120],
-        [120, 50, 120]]
-var = 64
+       [85, 60, 100],
+       [65, 100, 80],
+       [55, 120, 120],
+       [120, 50, 120]]
+var = 20
 std = np.sqrt(var)
 
-time_series = np.zeros(shape=(30*5,3), dtype=np.float64)
+time_series = np.zeros(shape=(30 * 5, 3), dtype=np.float64)
 for i, cluster in enumerate(mus):
     for j, mu in enumerate(cluster):
-        time_series[i*30:(i*30)+30, j] = np.random.normal(mu, std, size=30)
+        time_series[i * 30:(i * 30) + 30, j] = np.random.normal(mu, std, size=30)
 
 transformed_ts = time_series[:, 1:] - time_series[:, :-1]
 labels = np.arange(transformed_ts.shape[0])
@@ -20,10 +20,62 @@ np.random.shuffle(labels)
 
 # Parametrization of the voting pool
 k = 18
-s = 1
-m = 1
+s = 9
+m = 3
+size_dataset = len(transformed_ts)
+iterations = 100
+total_error = 0.
+total_ri = 0.
 
-clusters = cluster_ts(transformed_ts, labels, k, s, m)
 
-print(np.unique(clusters).shape[0])
-print(clusters)
+def count_misclassified(inferred_clusters):
+    misclassified = 0.
+    # first we count the nb of pairs that have correctly been put in the same cluster
+    for i in range(size_dataset):
+        # define i cluster
+        cluster1 = i // 30
+        for j in range(i + 1, size_dataset):
+            cluster2 = j // 30
+            if cluster1 == cluster2:
+                if inferred_clusters[i] != inferred_clusters[j]:
+                    # True positive
+                    misclassified += 1
+            else:
+                if inferred_clusters[i] == inferred_clusters[j]:
+                    # True negative
+                    misclassified += 1
+    return misclassified / ((size_dataset * (size_dataset - 1))/2)
+
+
+def rand_index(inferred_clusters):
+    TP = 0.
+    TN = 0.
+    # first we count the nb of pairs that have correctly been put in the same cluster
+    for i in range(size_dataset):
+        # define i cluster
+        cluster1 = i // 30
+        for j in range(i+1, size_dataset):
+            cluster2 = j // 30
+            if cluster1 == cluster2:
+                if inferred_clusters[i] == inferred_clusters[j]:
+                    # True positive
+                    TP += 1
+            else:
+                if inferred_clusters[i] != inferred_clusters[j]:
+                    # True negative
+                    TN += 1
+
+    return (TP + TN) / ((size_dataset * (size_dataset - 1))/2)
+
+
+if __name__ == "__main__":
+    # Calculate the Rand Index
+    for i in range(iterations):
+        new_labels = cluster_ts(transformed_ts, labels, k, s, m, max_iterations=500)
+        ri = rand_index(new_labels)
+        error = 1-ri
+        print('iteration {}: (ri {}, error {})'.format(i, ri, error))
+        total_error += error
+        total_ri += ri
+    print(total_error / iterations)
+    print(total_ri / iterations)
